7fc7e135110b4780fe5e42b21fd6fc97c10fde7f
hanifmz07 <13521157@std.stei.itb.ac.id>
2023-01-25 10:16:12 +0700

    Add random input feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,91 @@
 #include "24game.hpp"
 #include <sstream>
+#include <cstdlib>
 
 int main() {
 
+    int n;
+    std::string card = "";
     std::vector<double> input;
-    std::string card;
-    std::cout << "Welcome to 24 Game Solver. Type in the input with the format below.\nX X X X" << std::endl;
+    std::cout << "Welcome to 24 Game Solver." << std::endl;
+    std::cout << "Choose input:" << std::endl;
+    std::cout << "1. User input" << std::endl;
+    std::cout << "2. Random" << std::endl;
     while (true) {
-        std::getline(std::cin, card);
-        std::stringstream ss(card);
-        std::string str_num;
-        bool valid = true;
-        while (ss >> str_num) {
-            if (str_num == "A") {
-                input.push_back(1);
-            } else if (str_num == "J") {
-                input.push_back(11);
-            } else if (str_num == "Q") {
-                input.push_back(12);
-            } else if (str_num == "K") {
-                input.push_back(13);
-            } else if (str_num == "2" || str_num == "3" || str_num == "4" || str_num == "5" || str_num == "6" || str_num == "7" || str_num == "8" || str_num == "9" || str_num == "10") {
-                input.push_back(stoi(str_num));
-            } else {
-                std::cout << "Input not valid, please try again." << std::endl;
-                valid = false;
-                ss.clear();
-                input.clear();
+        std::cin >> n;
+        if (n == 1) {
+            std::cin.clear();
+            fflush(stdin);
+            std::cout << "Type in the input with the format below.\nX X X X" << std::endl;
+            while (true) {
+                
+                std::getline(std::cin, card);
+                std::stringstream ss(card);
+                std::string str_num;
+                
+                bool valid = true;
+                while (ss >> str_num) {
+                    if (str_num == "A") {
+                        input.push_back(1);
+                    } else if (str_num == "J") {
+                        input.push_back(11);
+                    } else if (str_num == "Q") {
+                        input.push_back(12);
+                    } else if (str_num == "K") {
+                        input.push_back(13);
+                    } else if (str_num == "2" || str_num == "3" || str_num == "4" || 
+                               str_num == "5" || str_num == "6" || str_num == "7" || 
+                               str_num == "8" || str_num == "9" || str_num == "10") {
+                        input.push_back(stoi(str_num));
+                    } else {
+                        std::cout << "Input not valid, please try again." << std::endl;
+                        valid = false;
+                        ss.clear();
+                        input.clear();
+                        break;
+                    }
+                }
+
+                if (!valid) {
+                    continue;
+                }
+
+                if (input.size() != 4) {
+                    std::cout << "Input not valid, please try again." << std::endl;
+                    ss.clear();
+                    input.clear();
+                    continue;
+                }
+
                 break;
             }
-        }
+            break;
 
-        if (!valid) {
-            continue;
-        }
+        } else if (n == 2) {
+            
+            srand(time(0));
+            for (int i = 0; i < 4; i++) {
+                input.push_back((rand() % 13) + 1);
+                if (input[i] == 1) {
+                    card += "A ";
+                } else if (input[i] == 11) {
+                    card += "J ";
+                } else if (input[i] == 12) {
+                    card += "Q ";
+                } else if (input[i] == 13) {
+                    card += "K ";
+                } else {
+                    card += std::to_string((int)input[i]) + " ";
+                }
+            }
+            card += "\n";
+            std::cout << card;
+            break;
 
-        if (input.size() != 4) {
+        } else {
             std::cout << "Input not valid, please try again." << std::endl;
-            ss.clear();
-            input.clear();
-            continue;
         }
 
-        break;
     }
     
     auto time1 = std::chrono::high_resolution_clock::now();
@@ -58,28 +101,38 @@ int main() {
     auto time2 = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(time2 - time1);
 
-    std::cout << count << " solution(s) found." << std::endl;
+    if (count == 0) {
+        std::cout << "No solution found." << std::endl;
+    } else {
+        std::cout << count << " solution(s) found." << std::endl;
+    }
 	std::cout << solution;
-    std::cout << "Time (ms): " << duration.count() << std::endl;
+    std::cout << "Time (microseconds): " << duration.count() << std::endl;
 
     char save;
     std::cout << "Store the solution in a .txt file?(y/n) ";
-    std::cin >> save;
     while (true) {
+        std::cin >> save;
 		if (save == 'y') {
-			std::string name;
+			
+            std::string name;
 			std::cout << "Input solution file name: ";
 			std::cin >> name;
 			std::ofstream FileSolution("test/" + name + ".txt");
 
-            FileSolution << card << std::endl;
-			FileSolution << count << " solution(s) found." << std::endl;
-			FileSolution << "Time (ms): " << duration.count() << std::endl;
+            FileSolution << card;
+            if (count == 0) {
+                FileSolution << "No solution found." << std::endl;
+            } else {
+                FileSolution << count << " solution(s) found." << std::endl;
+            }
+			FileSolution << "Time (microseconds): " << duration.count() << std::endl;
 			FileSolution << solution;
 			std::cout << "Success." << std::endl;
 			break;
 		} else if (save != 'y' && save != 'n') {
-			std::cout << "Input not valid." << std::endl;
+			
+            std::cout << "Input not valid, please try again." << std::endl;
 			continue;
 		}
 		break;
